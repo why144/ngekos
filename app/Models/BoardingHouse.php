0e487c75fb6ffa9ba9980fb94afb1850f746c9ed
why144 <wahyudi140319@gmail.com>
2025-09-27 14:33:54 +0700

    initial commit

---
+++ app/Models/BoardingHouse.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BoardingHouse extends Model
+{
+    protected $fillable = ['name', 'slug', 'thumbnail', 'city_id', 'category_id', 'description', 'price', 'address'];
+
+    public function city()
+    {
+        return $this->belongsTo(City::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
